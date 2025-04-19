2bfe651293b59ccf3c28203752a0a529d4c1749b
sowmik-sec <sowmik.sec@gmail.com>
2025-04-19 22:11:59 +0600

    Implementing Relation Filters

--- src/relational-queries.ts
+++ src/relational-queries.ts
@@ -4,18 +4,29 @@ const prisma = new PrismaClient();
 
 const relationalQueries = async () => {
   // fluent api
-  const result = await prisma.user
-    .findUnique({
-      where: {
-        id: 3,
-      },
-      // include: {
-      //   posts: true,
-      // },
-    })
-    .posts();
+  //   const result = await prisma.user
+  //     .findUnique({
+  //       where: {
+  //         id: 3,
+  //       },
+  //       // include: {
+  //       //   posts: true,
+  //       // },
+  //     })
+  //     .posts();
+  //   console.log(result);
 
-  console.log(result);
+  // relational filters
+  const publishedPostUsers = await prisma.user.findMany({
+    include: {
+      posts: {
+        where: {
+          published: true,
+        },
+      },
+    },
+  });
+  console.dir(publishedPostUsers, { depth: Infinity });
 };
 
 relationalQueries();
