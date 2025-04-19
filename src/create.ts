abee8776e5a3fb9dc61705f56a5bb8e3503f6cea
sowmik-sec <sowmik.sec@gmail.com>
2025-04-19 21:40:20 +0600

    Inserting Data into the Database with Relationships

--- src/create.ts
+++ src/create.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, UserRole } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
@@ -11,29 +11,72 @@ const main = async () => {
   //       published: true,
   //     },
   //   });
-  const createMany = await prisma.post.createMany({
-    data: [
-      {
-        title: "This is my third title",
-        content: "This is my third post",
-        authorName: "John Doe",
-        published: false,
+  // const createMany = await prisma.post.createMany({
+  //   data: [
+  //     {
+  //       title: "This is my third title",
+  //       content: "This is my third post",
+  //       authorName: "John Doe",
+  //       published: false,
+  //     },
+  //     {
+  //       title: "This is my fourth title",
+  //       content: "This is my fourth post",
+  //       authorName: "John Doe",
+  //       published: true,
+  //     },
+  //     {
+  //       title: "This is my fifth title",
+  //       content: "This is my fifth post",
+  //       authorName: "John Doe",
+  //       published: true,
+  //     },
+  //   ],
+  // });
+  // console.log(createMany);
+  // const createUser = await prisma.user.create({
+  //   data: {
+  //     username: "John Doe2",
+  //     email: "john@doe2.com",
+  //     role: UserRole.user,
+  //   },
+  // });
+  // console.log({ createUser });
+  // const createProfile = await prisma.profile.create({
+  //   data: {
+  //     bio: "This is my bio",
+  //     userId: 1,
+  //   },
+  // });
+  // console.log({ createProfile });
+  // const createCategory = await prisma.category.create({
+  //   data: {
+  //     name: "Software Engineering",
+  //   },
+  // });
+  // console.log({ createCategory });
+
+  const createPost = await prisma.post.create({
+    data: {
+      title: "This is post",
+      content: "This is content of the post",
+      authorId: 1,
+      postCategories: {
+        create: {
+          categoryId: 3,
+          // category: {
+          //   connect: {
+          //     id: 1,
+          //   },
+          // },
+        },
       },
-      {
-        title: "This is my fourth title",
-        content: "This is my fourth post",
-        authorName: "John Doe",
-        published: true,
-      },
-      {
-        title: "This is my fifth title",
-        content: "This is my fifth post",
-        authorName: "John Doe",
-        published: true,
-      },
-    ],
+    },
+    include: {
+      postCategories: true,
+    },
   });
-  console.log(createMany);
+  console.log(createPost);
 };
 
 main();
