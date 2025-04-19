2fa53e38c532ddd2bff623cf6d120d67476b77c2
sowmik-sec <sowmik.sec@gmail.com>
2025-04-19 19:20:50 +0600

    Implement delete functionality for unpublished posts and update dev script

---
+++ src/delete.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const deleteData = async () => {
+  //   const singleDelete = await prisma.post.delete({
+  //     where: {
+  //       id: 1,
+  //     },
+  //   });
+  //   console.log(singleDelete);
+  const deleteMany = await prisma.post.deleteMany({
+    where: {
+      published: false,
+    },
+  });
+  console.log(deleteMany);
+};
+
+deleteData();
