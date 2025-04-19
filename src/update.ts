ffe8b7d9fa4b80889e1efb27903e13c3f1719268
sowmik-sec <sowmik.sec@gmail.com>
2025-04-19 18:36:02 +0600

    Updating Records: Single and Multiple Record Updates and Their Distinctions

---
+++ src/update.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const updates = async () => {
+  //   const singleUpdate = await prisma.post.update({
+  //     where: {
+  //       id: 5,
+  //     },
+  //     data: {
+  //       //   title: "Title 5",
+  //       //   content: "Content 5",
+  //       //   authorName: "Author 5",
+  //       published: false,
+  //     },
+  //   });
+  //   console.log(singleUpdate);
+  const updateMany = await prisma.post.updateMany({
+    where: {
+      title: "Title 1",
+    },
+    data: {
+      published: true,
+    },
+  });
+  console.log(updateMany);
+};
+
+updates();
