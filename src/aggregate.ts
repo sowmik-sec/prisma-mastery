3cca5d1f944613f91843df63eac534ed320239dd
sowmik-sec <sowmik.sec@gmail.com>
2025-04-20 11:16:11 +0600

    Sum, Count, Maximum, Minimum Aggregation

--- src/aggregate.ts
+++ src/aggregate.ts
@@ -8,7 +8,29 @@ const aggregate = async () => {
       age: true,
     },
   });
-  console.log(avgAge);
+  //   console.log(avgAge);
+  const sumAge = await prisma.user.aggregate({
+    _sum: {
+      age: true,
+    },
+  });
+  const countAge = await prisma.user.aggregate({
+    _count: {
+      age: true,
+    },
+  });
+  const countData = await prisma.user.count();
+  const maxAge = await prisma.user.aggregate({
+    _max: {
+      age: true,
+    },
+  });
+  const minAge = await prisma.user.aggregate({
+    _min: {
+      age: true,
+    },
+  });
+  console.log(maxAge, minAge);
 };
 
 aggregate();
